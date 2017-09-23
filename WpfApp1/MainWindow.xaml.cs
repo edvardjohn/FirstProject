49d7c36c7d4fd7d41a8d2fa110bcfb1b0b949f7e
edvardjohn <edvard.j.collin@gmail.com>
2017-09-23 18:08:56 +0200

    Added DÅ

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -27,7 +27,7 @@ namespace WpfApp1
 
         private void button_Click(object sender, RoutedEventArgs e)
         {
-           ButtonTextBox.Text += "Hej ";
+           ButtonTextBox.Text += "Hej DÅ!";
         }
     }
 }
